2ac4e12d351325f24923fcf7123a234f7ae915bc
Akash Singh <akashsingh.work@outlook.com>
2020-06-20 15:34:28 +0530

    Adding exercise file

---
+++ Basics/Questions6.java
@@ -0,0 +1,12 @@
+package Basics;
+
+public class Questions6 {
+    public static void main(String []args){
+        String firstName = "Akash";
+        String lastName = "Singh";
+        String fullName = firstName +" "+ lastName;
+        int num = fullName.length();
+        System.out.println("Hello, my name is "+fullName);
+        System.out.println("There are " + num + " letters in my name");
+    }
+}
--- custom_order.java
+++ custom_order.java
@@ -30,20 +30,13 @@ double tax; // Amount of tax
      System.out.println("Welcome to Java's Cake & Cupcake Shop!");
     System.out.println("We make custom cakes with our secret cake batter!"); 
 
-	  
-	  
-    // TEST CODE
-    
     // STEP 3 INPUT YOUR NAME AND PREPARE TO VIEW MENU
   System.out.print("What is your first name? ");
  firstName = keyboard.nextLine();
 
 System.out.print(firstName + ", please see our MENU below: ");
 System.out.print("\n"); // skips a line
-	  
-      
-    // TEST CODE     
-    
+
     // STEP 4 DISPLAY MENU
 System.out.println("_______________________________________________");      
 System.out.println("        MENU         QUANTITY    BASE COST  ");
@@ -55,52 +48,22 @@ System.out.println("Frostings (vanilla, chocolate, strawberry, coco)");
 System.out.println("Fillings (mocha, mint, lemon, caramel, vanilla)");
 System.out.println("Toppings (sprinkles, cinnamon, cocoa, nuts)");
 System.out.println("_______________________________________________");
-	
-      
 
-    // TEST CODE     
-    
     // STEP 5 PROMPT USER TO ORDER
 System.out.println("Do you want CUPCAKES or a CAKE?");
 itemOrder = keyboard.nextLine();
-  
-	  
-	  
-	  
-      
-    // TEST CODE
-      
     // STEP 6 PROMPT USER TO CHOOSE FROSTING
 System.out.println("What type of FROSTING do you want? ");
 System.out.println("Vanilla, Chocolate, Strawberry or Coco");
 frostingType = keyboard.nextLine();
-   
-	 
-	 
-	 
-      
-    //TEST CODE
-      
     // STEP 7 PROMPT USER TO CHOOSE FILLING
 System.out.println("What type of FILLING do you want? ");
 System.out.println("Mocha, Mint, Lemon, Caramel or Raspberry");
 fillingType = keyboard.nextLine();
-
-	  
-	  
-      
-    // TEST CODE
-      
     // STEP 8 PROMPT USER TO CHOOSE TOPPINGS
 System.out.println("What type of TOPPINGS do you want? ");
 System.out.println("Sprinkles, Cinnamon, Cocoa, Nuts");
 toppings = keyboard.nextLine();
-
-	
-      
-      
-    // TEST CODE
-      
     // STEP 9 DISPLAY ORDER CONFIRMATION
 System.out.println();
 System.out.println(firstName + " , your order is as follows: ");
@@ -110,14 +73,6 @@ System.out.println("Frosting: " + frostingType);
 System.out.println("Filling: " + fillingType);
 System.out.println("Toppings: " + toppings);
 System.out.println("_________________________________________");
-
-      
-	  
-	  
-	  
-    
-    // TEST CODE
-      
     // STEP 10 DISPLAY COST AND SALES TAX
 System.out.printf("The cost of your order is: $%.2f\n", cost);
 tax = cost * TAX_RATE;
